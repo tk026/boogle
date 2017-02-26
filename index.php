27524032961f4a2aa01747818ddcd1a56c80389d
mrurlwin <nz_danny_boy@hotmail.com>
2017-02-26 22:56:06 +1300

    First commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en-nz">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Boogle</title>
+
+  <link rel="stylesheet" type="text/css" href="css/app.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <h1>
+        <a href="index.php">
+          <img class="logo" src="img/logo.png"/>
+        </a>
+      </h1>
+    </div>
+    <div class="row">
+      <div class="col-md-2"></div>
+      <div class="col-md-8 col-sm-12">
+          <div class="field" id="searchform">
+            <input type="text" name="q" autofocus required />
+
+            <button type="submit" id="search">Search</button>
+          </div>
+      </div>
+      <div class="col-md-2"></div>
+    </div>
+    <div class="row">
+      <div class="col-md-3"></div>
+      <div class="col-md-6 links">
+      <a href="#">
+          I'm feeling lucky
+        </a>
+      </div>
+      <div class="col-md-3"></div>
+    </div>
+  </div>
+</body>
+</html>
---
+++ results.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en-nz">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Boogle</title>
+
+  <link rel="stylesheet" type="text/css" href="css/app.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+  <nav class="navbar navbar-default navbar-static-top">
+    <div class="container">
+      <div class="navbar-header">
+        <!-- Collapsed Hamburger -->
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+          <span class="sr-only">Toggle Navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+
+        <!-- Branding Image -->
+        <a class="navbar-brand" href="index.php">
+         <img width="80px" class="logo-top" src="img/logo.png"/>
+       </a>
+     </div>
+
+     <div class="collapse navbar-collapse" id="app-navbar-collapse">
+      <!-- Left Side Of Navbar -->
+      <ul class="nav navbar-nav">
+        <li>
+          <div class="field top-bar-search" id="searchform">
+            <input type="text" name="q" autofocus required value="$searchQuery" />
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- Content -->
+<div class="container">
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2">
+      <h4>Search results for <strong>$searchQuery</strong></h4>
+      <!-- Displayed results -->
+      <ul class="list-group">
+        <!-- Single Result -->
+        <li class="list-group-item notification-bar-fail m-b-1">
+          <div href="#" class="notification-bar-icon">
+            <div>
+              <i></i>
+            </div>
+          </div>
+
+          <div class="notification-bar-details">
+          <a href="$url" class="notification-bar-title">
+              $searchQuery['text']
+            </a>
+            <span class="text-muted">$searchQuery['url']</span>
+          </div>
+        </li>
+        <!-- End of single result -->
+        <li class="list-group-item notification-bar-fail m-b-1">
+          <div href="#" class="notification-bar-icon">
+            <div>
+              <i></i>
+            </div>
+          </div>
+
+          <div class="notification-bar-details">
+          <a href="$url" class="notification-bar-title">
+              $searchQuery['text']
+            </a>
+            <span class="text-muted">$searchQuery['url']</span>
+          </div>
+        </li>
+        <!-- End of single result -->
+        <li class="list-group-item notification-bar-fail m-b-1">
+          <div href="#" class="notification-bar-icon">
+            <div>
+              <i></i>
+            </div>
+          </div>
+
+          <div class="notification-bar-details">
+          <a href="$url" class="notification-bar-title">
+              $searchQuery['text']
+            </a>
+            <span class="text-muted">$searchQuery['url']</span>
+          </div>
+        </li>
+        <!-- End of single result -->
+      </ul>
+    </div>
+  </div>
+</div>
+<div class="footer-bottom">
+  <div class="container">
+    <p class="pull-left"> Copyright &copy; Boogle 2017. All rights reserved. </p>
+    <div class="pull-right">
+      <ul class="nav nav-pills">
+        <li><a href="feedback.php">Feedback</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+</body>
+</html>
