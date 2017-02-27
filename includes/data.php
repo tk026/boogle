ae386e3e2eec5e4c4cdaf833fd1442da6615f171
mrurlwin <nz_danny_boy@hotmail.com>
2017-02-27 17:02:13 +1300

    Add a new data file,  filter the array, show an error if no results

---
+++ includes/data.php
@@ -0,0 +1,107 @@
+<?php
+return array(
+  array(
+        'text' =>'Unicode snowman for you',
+        'url' =>'http://unicodesnowmanforyou.com/'
+        ),
+  array(
+        'text' =>'The UK, the greatest little country on earth and good friends with',
+        'url' =>'http://brexit.de/'
+        ),
+  array(
+        'text' =>'Can I fly a kite',
+        'url' =>'https://www.caniflyakite.com/'
+        ),
+  array(
+        'text' =>'Eel slap!',
+        'url' =>'http://eelslap.com/'
+        ),
+  array(
+        'text' =>'Ham rocks',
+        'url' =>'https://ham.rocks/'
+        ),
+  array(
+        'text' =>'My life is a loop :(',
+        'url' =>'http://www.molleindustria.org/everydaythesamedream/everydaythesamedream.html'
+        ),
+  array(
+        'text' =>'Movie in a minute',
+        'url' =>'http://www.rinkworks.com/movieaminute/'
+        ),
+  array(
+        'text' =>'Million dollar homepage',
+        'url' =>'http://www.milliondollarhomepage.com/'
+        ),
+  array(
+        'text' =>'Cow Spinner',
+        'url' =>'http://spinnymeat.com/'
+        ),
+  array(
+        'text' =>'Museum of selfies',
+        'url' =>'http://museumofselfies.tumblr.com/'
+        ),
+  array(
+        'text' =>'Chill out and watch some cat gifs',
+        'url' =>'http://chilloutandwatchsomecatgifs.com/'
+        ),
+  array(
+        'text' =>'Bouncy balls...',
+        'url' =>'https://bouncyballs.org/'
+        ),
+  array(
+        'text' =>'You crazy (cray cray)',
+        'url' =>'http://youcraycray.com/'
+        ),
+  array(
+        'text' =>'Flying Opera Pigs',
+        'url' =>'http://flyingoperapigs.com/'
+        ),
+  array(
+        'text' =>'Just a bell',
+        'url' =>'http://hectorsalamanca.com/'
+        ),
+  array(
+        'text' =>'Leak spin',
+        'url' =>'http://leekspin.com/'
+        ),
+  array(
+        'text' =>'D-E-F-I-N-I-T-E-L-Y',
+        'url' =>'http://d-e-f-i-n-i-t-e-l-y.com/'
+        ),
+  array(
+        'text' =>'Cat party',
+        'url' =>'http://www.catparty.nonuigi.moe/'
+        ),
+  array(
+        'text' =>'Bacon sizzling',
+        'url' =>'http://www.baconsizzling.com/'
+        ),
+  array(
+        'text' =>'Chat with yourself or watch...',
+        'url' =>'http://www.chatwithyourself.com/'
+        ),
+  array(
+        'text' =>'Pls pet doge',
+        'url' =>'http://www.plspetdoge.com/'
+        ),
+  array(
+        'text' =>'How secure is my password',
+        'url' =>'https://howsecureismypassword.net/'
+        ),
+  array(
+        'text' =>'Polar bear in a snow storm',
+        'url' =>'http://www.polarbearinasnowstorm.com/'
+        ),
+  array(
+        'text' =>'Dotted Lines',
+        'url' =>'http://www.dottedlines.com/'
+        ),
+  array(
+        'text' =>'Welcome to the internet setup!',
+        'url' =>'https://html.house/jnftbgke.html'
+        ),
+  array(
+        'text' =>'Test lol',
+        'url' =>'https://www.testlol.com/'
+        )
+  );
--- results.php
+++ results.php
@@ -1,21 +1,24 @@
 <?php
+  // Save the query submitted to a variable
   $searchQuery = $_GET['q'];
 
-  $data = array(
-    array(
-            'text' => 'Apple',
-            'url' => 'http://apple.com'
-          ),
-    array(
-            'text' => 'Bakers Delight',
-            'url' => 'http://bakersdelight.co.nz'
-          )
-    );
-    // die(var_dump($data));
-  foreach ($data as $term) {
-      echo '<a href="'. $term['url'] .'">'. $term['text'] .'</a><br>';
-  }
-  die();
+  // Pull in the large data array
+  $data = require 'includes/data.php';
+
+  // This magical function searches through an array
+  function filterResults($searchQuery, $data) {
+          $matches = [];
+
+          foreach ($data as $key => $value) {
+            if (strpos(strtolower($value['text']), strtolower($searchQuery)) !== false) {
+              $matches[] = $value;
+            }
+          }
+          return $matches;
+      }
+
+  // Here is where the magical function is called and returns the results.
+  $terms = filterResults($searchQuery, $data);
 
   require 'partials/header.php';
   require 'partials/navigation.php';
@@ -27,52 +30,49 @@
       <h4>Search results for <strong><?= $searchQuery; ?></strong></h4>
       <!-- Displayed results -->
       <ul class="list-group">
-        <!-- Single Result -->
-        <li class="list-group-item notification-bar-fail m-b-1">
-          <div href="#" class="notification-bar-icon">
-            <div>
-              <i></i>
-            </div>
-          </div>
+        <?php
+        // If there are no results...
+        if (empty($terms)):
+        ?>
+          <li class="list-group-item notification-bar-fail m-b-1">
 
-          <div class="notification-bar-details">
-          <a href="$url" class="notification-bar-title">
-              $searchQuery['text']
-            </a>
-            <span class="text-muted">$searchQuery['url']</span>
-          </div>
-        </li>
-        <!-- End of single result -->
-        <li class="list-group-item notification-bar-fail m-b-1">
-          <div href="#" class="notification-bar-icon">
-            <div>
-              <i></i>
-            </div>
-          </div>
 
-          <div class="notification-bar-details">
-          <a href="$url" class="notification-bar-title">
-              $searchQuery['text']
-            </a>
-            <span class="text-muted">$searchQuery['url']</span>
-          </div>
-        </li>
-        <!-- End of single result -->
-        <li class="list-group-item notification-bar-fail m-b-1">
-          <div href="#" class="notification-bar-icon">
-            <div>
-              <i></i>
-            </div>
-          </div>
+            <div class="notification-bar-details">
+              <h3 class="notification-bar-title">
+                Nothing was found :(<br>
+              </h3>
+              <p>
+                <strong>Suggestions:</strong>
+                <ul>
+                  <li>Make sure that all words are spelled correctly.</li>
+                  <li>Try different keywords.</li>
+                  <li>Try more general keywords.</li>
+                </ul>
 
-          <div class="notification-bar-details">
-          <a href="$url" class="notification-bar-title">
-              $searchQuery['text']
-            </a>
-            <span class="text-muted">$searchQuery['url']</span>
-          </div>
-        </li>
-        <!-- End of single result -->
+              </p>
+            </div>
+          </li>
+          <!-- End of single result -->
+        <?php
+
+        // If there are results
+        else:
+          foreach ($terms as $term):
+          ?>
+          <!-- Single Result -->
+          <li class="list-group-item notification-bar-fail m-b-1">
+            <div class="notification-bar-details">
+            <a href="<?= $term['url'] ?>" class="notification-bar-title">
+                <?= $term['text'] ?>
+              </a>
+              <span class="text-muted"><?= $term['url'] ?></span>
+            </div>
+          </li>
+          <!-- End of single result -->
+          <?php
+          endforeach;
+        endif;
+        ?>
       </ul>
     </div>
   </div>
