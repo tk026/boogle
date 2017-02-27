e27c230f81f331bb988fb8493ac9ba129ddefc6a
mrurlwin <nz_danny_boy@hotmail.com>
2017-02-27 17:12:19 +1300

    Sample pages for adding the feedback form.

---
+++ feedback.php
@@ -0,0 +1,11 @@
+<?php include 'partials/header.php'; ?>
+
+<h1>
+  <a href="index.php">
+    <img class="logo" src="img/logo.png"/>
+  </a>
+</h1>
+
+<!-- Here is where you'll write your form... -->
+
+<?php include 'partials/footer.php'; ?>
---
+++ thanks.php
@@ -0,0 +1,15 @@
+<?php include 'partials/header.php'; ?>
+
+<h1>
+  <a href="index.php">
+    <img class="logo" src="img/logo.png"/>
+  </a>
+</h1>
+
+<h2 class="text-center">Thanks for the feedback <?= $name ?></h2>
+<h2 class="text-center text-muted">We will reply to <?= $email ?> if we need to follow up.</h2>
+<div class="text-center">
+  <a href="index.php">Home</a>
+</div>
+
+<?php include 'partials/footer.php'; ?>
